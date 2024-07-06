b53fe54b4deea64f9585f60fe110527d58b7ad62
AButterworth0 <andrewbutterworth0@gmail.com>
2024-07-06 15:56:56 +0100

    feat: first failing test

--- holiday_search.tests/HolidaySearchTests.cs
+++ holiday_search.tests/HolidaySearchTests.cs
@@ -12,23 +12,37 @@ namespace holiday_search.tests
         //  Duration: 7 nights
         //Expected:
         //  Flight 2 and Hotel 9
+        [Fact]
+        public void GivenExactDepartingAirportReturnBestValueHoliday()
+        {
+            //arrange
+            string expected = "Flight 2 and Hotel 9";
+            SearchInput searchInput = new SearchInput() { DepartingFrom = "MAN", TravellingTo = "AGP", DepartureDate= new DateTime(2023,07,01), DurationInNights = 7};
+            HolidaySearch holidaySearch = new HolidaySearch(searchInput);
 
-        //Customer 2
-        //Inputs:
-        //  Departing from: Any London Airport
-        //  Traveling to: Mallorca Airport (PMI)
-        //  Departure Date: 2023/06/15
-        //  Duration: 10 nights
-        //Expected:
-        //  Flight 6 and Hotel 5 
+        //act
+        string actual = holidaySearch.GetBestValueHoliday();
 
-        //Customer 3
-        //Inputs:
-        //  Departing from: Any Airport 
-        //  Traveling to: Gran Canaria Airport(LPA)
-        //  Departure Date: 2022/11/10
-        //  Duration: 14 nights
-        // Expected:
-        //  Flight 7 and Hotel 6 
-    }
+        //assert
+        Assert.Equal(expected, actual);
+        }
+
+    //Customer 2
+    //Inputs:
+    //  Departing from: Any London Airport
+    //  Traveling to: Mallorca Airport (PMI)
+    //  Departure Date: 2023/06/15
+    //  Duration: 10 nights
+    //Expected:
+    //  Flight 6 and Hotel 5 
+
+    //Customer 3
+    //Inputs:
+    //  Departing from: Any Airport 
+    //  Traveling to: Gran Canaria Airport(LPA)
+    //  Departure Date: 2022/11/10
+    //  Duration: 14 nights
+    // Expected:
+    //  Flight 7 and Hotel 6 
+}
 }
---
+++ holiday_search/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class HolidaySearch {
+
+    public HolidaySearch(SearchInput searchInput)
+    {
+
+    }
+
+    public string GetBestValueHoliday()
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+public class SearchInput {
+    public string DepartingFrom { get; set; }
+    public string TravellingTo { get; set; }
+    public DateTime DepartureDate { get; set; }
+    public int DurationInNights { get; set; }
+}
