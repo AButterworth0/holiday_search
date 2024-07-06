9bdc54f6249a9c2ca0ac9e7632444935af1ffd5b
AButterworth0 <andrewbutterworth0@gmail.com>
2024-07-06 15:17:30 +0100

    docs: add test list

--- holiday_search.tests/HolidaySearchTests.cs
+++ holiday_search.tests/HolidaySearchTests.cs
@@ -1,7 +1,34 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace holiday_search.tests
 {
     public class HolidaySearchTests
     {
-        
+        //Customer 1
+        //Inputs:
+        //  Departing from: Manchester Airport(MAN)
+        //  Traveling to: Malaga Airport(AGP)
+        //  Departure Date: 2023/07/01 
+        //  Duration: 7 nights
+        //Expected:
+        //  Flight 2 and Hotel 9
+
+        //Customer 2
+        //Inputs:
+        //  Departing from: Any London Airport
+        //  Traveling to: Mallorca Airport (PMI)
+        //  Departure Date: 2023/06/15
+        //  Duration: 10 nights
+        //Expected:
+        //  Flight 6 and Hotel 5 
+
+        //Customer 3
+        //Inputs:
+        //  Departing from: Any Airport 
+        //  Traveling to: Gran Canaria Airport(LPA)
+        //  Departure Date: 2022/11/10
+        //  Duration: 14 nights
+        // Expected:
+        //  Flight 7 and Hotel 6 
     }
 }
