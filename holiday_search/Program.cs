6877228f1cf7f422e65de3552e0e99b050e37737
AButterworth0 <andrewbutterworth0@gmail.com>
2024-07-07 16:49:43 +0100

    feat: add models

--- holiday_search/Program.cs
+++ holiday_search/Program.cs
@@ -21,3 +21,30 @@ public class SearchInput {
     public DateTime DepartureDate { get; set; }
     public int DurationInNights { get; set; }
 }
+
+public class Hotel {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime ArrivalDate { get; set;}
+    public Decimal PricePerNights { get; set; }
+    public List<string> LocalAirports { get; set; }
+}
+
+public class Flights
+{
+    public int Id { get; set; }
+    public string Airline { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public Decimal Price { get; set; }
+    public DateTime DepartureDate { get; set; }
+}
+
+public class Airport
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
+
